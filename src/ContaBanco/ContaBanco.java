97c9f8b141d3a764974445a23fe08e77b6055d97
clebermarcolino <cleber.marcolino@dcx.ufpb.br>
2023-07-17 16:57:43 -0300

    Conta Bancária

---
+++ src/ContaBanco/ContaBanco.java
@@ -0,0 +1,126 @@
+package ContaBanco;
+
+public class ContaBanco {
+    public int numConta;
+    protected String tipoConta;
+    private String titular;
+    private double saldo;
+    private boolean status;
+
+    public ContaBanco() {
+        saldo = 0;
+        status = false;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        if(numConta <= 0) {
+            throw new RuntimeException("Número de conta inválido");
+        }
+        else {
+            this.numConta = numConta;
+        }
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        if(!tipoConta.equals("cp") && !tipoConta.equals("cc")) {
+            throw new RuntimeException("Tipo de conta inválida! Digite cc para conta corrente e cp para conta poupança. ");
+        }
+        else {
+            this.tipoConta = tipoConta;
+        }
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void abrirConta(String tipo) {
+        setTipoConta(tipo);
+        setStatus(true);
+
+        if(tipo.equals("cp")) {
+            setSaldo(getSaldo() + 150);
+        }
+        else {
+            setSaldo(getSaldo() + 50);
+        }
+        System.out.println("Conta aberta com sucesso. ");
+    }
+    public void fecharConta() {
+        if(saldo > 0) {
+            System.out.println("Não é possível fechar a conta, pois a conta contém saldo. ");
+        }
+        else {
+            setStatus(false);
+            System.out.println("Conta fechada com sucesso. ");
+        }
+    }
+
+    public void depositar(double valorDeposito) {
+        if(status && valorDeposito > 0) {
+            setSaldo(getSaldo() + valorDeposito);
+            System.out.println("Valor depositado com sucesso! ");
+        }
+        else {
+            System.out.println("Não foi possível efetuar o depósito! ");
+        }
+    }
+
+    public void sacar(double valorSaque) {
+        if(status && valorSaque <= saldo) {
+            setSaldo(getSaldo() - valorSaque);
+            System.out.println("Saque depositado com sucesso! ");
+        }
+        else {
+            System.out.println("Não foi possível efetuar o saque! ");
+        }
+    }
+
+    public void pagarMensal() {
+        if(saldo <= 0) {
+            System.out.println("Sem saldo! ");
+        }
+        else if(tipoConta.equals("cc") && saldo >= 12) {
+            setSaldo(getSaldo() - 12);
+            System.out.println("Mensalidade de 12 reais! ");
+        } else if(tipoConta.equals("cp") && saldo >= 20) {
+            setSaldo(getSaldo() - 20);
+            System.out.println("Mensalidade de 20 reias");
+        }
+        else {
+            System.out.println("Não há saldo suficiente! ");
+        }
+    }
+
+
+
+
+}
---
+++ src/ContaBanco/Main.java
@@ -0,0 +1,65 @@
+package ContaBanco;
+
+import ContaBanco.ContaBanco;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int operacao;
+
+        ContaBanco conta = new ContaBanco();
+        Scanner sc = new Scanner(System.in);
+
+        // Abertura da conta bancária logo abaixo
+
+        conta.setNumConta(392);  // Número de conta
+        conta.setTitular("Clayton"); // Titular da conta
+        conta.abrirConta("cp"); // Tipo da conta. cc para Conta corrente ou cp para Conta Poupança
+
+        do {
+            System.out.print("""
+                    Digite abaixo o número correspondente a operação que você deseja realizar:
+                    1 = Depositar
+                    2 = Sacar
+                    3 = Pagar Mensal
+                    4 = Fechar Conta
+                    """);
+
+            operacao = sc.nextInt();
+            switch (operacao) {
+                case 1:
+                    System.out.println("Qual valor deseja depositar? ");
+                    double deposito = sc.nextDouble();
+                    conta.depositar(deposito);
+                    System.out.printf("Saldo: %.2f %n",conta.getSaldo());
+                    break;
+
+                case 2:
+                    System.out.println("Qual valor deseja sacar? ");
+                    double saque = sc.nextDouble();
+                    conta.sacar(saque);
+                    System.out.printf("Saldo: %.2f %n",conta.getSaldo());
+                    break;
+
+                case 3:
+                    conta.pagarMensal();
+                    System.out.printf("Saldo: %.2f %n",conta.getSaldo());
+                    break;
+
+                case 4:
+                    System.out.println("Número da conta é: " + conta.getNumConta());
+                    System.out.println("Titular: " + conta.getTitular());
+                    System.out.println("Saldo: " + conta.getSaldo());
+                    System.out.println("Tipo de Conta: " + conta.getTipoConta());
+                    System.out.println("Status da conta: " + conta.isStatus());
+                    conta.fecharConta();
+                    break;
+
+                default:
+                    throw new RuntimeException("Operação Inválida ! ");
+            }
+        } while (operacao != 4);
+        sc.close();
+    }
+}
